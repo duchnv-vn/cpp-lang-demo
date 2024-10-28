66c169e023e9019701cc9090e4509d9726ba05a1
duchnv-vn <huynhnguyenvietduc@gmail.com>
2024-10-28 22:44:42 +0700

    learn-cpp-complete-course: data types & input

---
+++ src/educative/learn-cpp-complete-course/section-4/main.cpp
@@ -0,0 +1,13 @@
+// ***************************************
+// DATA TYPES & INPUT
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    double doubleNum = 97e7;
+
+    char char1 = 'C';
+
+    bool isTrue = true;
+}
