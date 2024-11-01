e7d5147044bce54fb591495f78f39718cc088f47
duchnv-vn <huynhnguyenvietduc@gmail.com>
2024-11-02 00:20:59 +0700

    learn-cpp-complete-course: data type modifier & type casting

--- src/educative/learn-cpp-complete-course/section-4/main.cpp
+++ src/educative/learn-cpp-complete-course/section-4/main.cpp
@@ -5,9 +5,39 @@ using namespace std;
 
 int main(void)
 {
-    double doubleNum = 97e7;
+    /*
+    Premitive data types
+     */
+    if (0)
+    {
+        double doubleNum = 97e7;
+        char char1 = 'C';
+        bool isTrue = true;
+    }
 
-    char char1 = 'C';
+    /*
+    Data type modifiers
+     */
+    if (0)
+    {
+        long long int number = 2147483649;
+        cout << number << endl;
+    }
 
-    bool isTrue = true;
+    /*
+    Type casting
+     */
+    if (1)
+    {
+        int int_value = 13.9;
+        cout << int_value << endl; // 13
+
+        int int_value_2 = 13.1;
+        cout << int_value_2 << endl; // 13
+
+        char character = 'A';
+        int ASCII;
+        ASCII = (int)character;
+        cout << ASCII << endl; // 65
+    }
 }
