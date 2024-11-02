bede2f17e5f277800e2ec4a02d4b2b33d0579bb1
duchnv-vn <huynhnguyenvietduc@gmail.com>
2024-11-02 12:35:20 +0700

    learn-cpp-complete-course: string & escape sequence & user input

--- src/educative/learn-cpp-complete-course/section-4/main.cpp
+++ src/educative/learn-cpp-complete-course/section-4/main.cpp
@@ -3,32 +3,28 @@
 #include <iostream>
 using namespace std;
 
-int main(void)
-{
+int main() {
     /*
-    Premitive data types
+    *  Primitive data types
      */
-    if (0)
-    {
+    if (false) {
         double doubleNum = 97e7;
         char char1 = 'C';
         bool isTrue = true;
     }
 
     /*
-    Data type modifiers
+    * Data type modifiers
      */
-    if (0)
-    {
+    if (false) {
         long long int number = 2147483649;
         cout << number << endl;
     }
 
     /*
-    Type casting
+    * Type casting
      */
-    if (1)
-    {
+    if (false) {
         int int_value = 13.9;
         cout << int_value << endl; // 13
 
@@ -37,7 +33,32 @@ int main(void)
 
         char character = 'A';
         int ASCII;
-        ASCII = (int)character;
+        ASCII = (int) character;
         cout << ASCII << endl; // 65
+    }
+
+    /*
+     * String & Escape Sequences
+     */
+    if (false) {
+        const string text = "Hello World!";
+        cout << text << endl;
+    }
+
+    /*
+     * User input
+     */
+    if (false) {
+        int inputValue;
+
+        cout << "Enter an integer: " << endl;
+        cin >> inputValue;
+        cout << "You have entered: " << inputValue << endl;
+    }
+
+    if (false) {
+        double double_value = 70.986;
+        int int_value = (int) double_value;
+        cout << int_value << endl;
     }
 }
